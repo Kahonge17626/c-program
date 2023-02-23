0be5260824543ee535f58240dce93724fc333e94
Kahonge17626 <122534502+Kahonge17626@users.noreply.github.com>
2023-02-23 15:36:44 +0300

    Add files via upload

---
+++ naturalnumbersum.c
@@ -0,0 +1,14 @@
+//Sum of Natural numbers
+#include<stdio.h>
+int main()
+{
+    int i, sum=0;
+    for (i=0; i<=100;i++){
+     printf("%d", i);
+     sum = sum + 1;
+        
+    }
+    printf("\n the sum is %d",sum);
+    
+    return 0;
+}
