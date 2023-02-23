30df81b3138dfedf871b828c492ac474b4978648
Kahonge17626 <122534502+Kahonge17626@users.noreply.github.com>
2023-02-23 13:26:32 +0300

    Add files via upload

---
+++ reversenumbers.c
@@ -0,0 +1,10 @@
+/*Program to display natural numbers inn reverse order*/
+#include<stdio.h>
+int main(){
+    int n;
+    for (n>=0; n<=50; n++){
+        printf("%d\n",n);
+    }
+    
+    return 0;
+}
